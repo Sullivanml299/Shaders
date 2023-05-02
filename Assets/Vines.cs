d70ff218f4e4be9423d0b9dad0b212a8bfaa7954
Marcus Sullivan <sullivanml299@gmail.com>
2023-05-02 15:51:57 -0400

    working on grow animation

--- Assets/Vines.cs
+++ Assets/Vines.cs
@@ -6,7 +6,9 @@ public class Vines : MonoBehaviour
 {
     public Material vineMaterial;
     public float radius = 0.1f;
+    public int splineSegmentResolution = 10; // number of points to generate along the spline for each segment
     public int numVertices = 10; // number of vertices that make up each ring
+    public float growthTime = 2f; // time it takes for the vine to grow from start to end
     public LineRenderer lineRenderer;
     public bool debugDraw = true;
     public bool drawMesh = true;
@@ -15,6 +17,12 @@ public class Vines : MonoBehaviour
     List<Vector3> vertices = new List<Vector3>();
     List<int> indices = new List<int>();
     Vector3[] pathPoints;
+    float growthInterval; // time between each segment growth
+    [Range(0f, 0.5f)] //TODO: remove this
+    public float ellapsedTime = 0f;
+    [Range(0f, 1f)]
+    public float lerpTest = 0f;
+
 
 
     // Start is called before the first frame update
@@ -23,18 +31,112 @@ public class Vines : MonoBehaviour
         mesh = new Mesh();
         var controlPoints = new Vector3[lineRenderer.positionCount];
         lineRenderer.GetPositions(controlPoints);
-        pathPoints = Spline.generateCatmullrom(controlPoints, 10).ToArray(); //FIXME: .toArray is inefficient
-        makeCylinderMesh();
+        pathPoints = Spline.generateCatmullrom(controlPoints, splineSegmentResolution).ToArray(); //FIXME: .toArray is inefficient
+
+        growthInterval = growthTime / pathPoints.Length;
+        print(growthInterval);
+        // makeCylinderMesh();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ellapsedTime < growthTime)
+        {
+            updateVineMeshPush();
+        }
         if (drawMesh) Graphics.DrawMesh(mesh, transform.localToWorldMatrix, vineMaterial, LayerMask.NameToLayer("Default"));
     }
 
     /// <summary>
-    /// creates a cylinder mesh along the path defined by pathPoints
+    /// creates a growing mesh that extend along the path defined by pathPoints
+    /// over time.
+    /// Adds vertices to the base of the vine. Pushes old vertices along the vine. 
+    /// </summary>
+    void updateVineMeshPush()
+    {
+        // use ellapsedTime to determine number of segments
+        int numSegments = Mathf.FloorToInt(ellapsedTime / growthInterval);
+
+        for (var i = 0; i < numSegments; i++)
+        {
+            if (vertices.Count < numSegments * numVertices)
+            {
+                // add new vertices to the base of the vine
+                Vector3 start = pathPoints[0];
+                Vector3 end = pathPoints[1];
+                makeRing(start, end, true);
+            }
+            else
+            {
+                // push old vertices along the vine
+                Vector3 start = pathPoints[numSegments - i - 1];
+                Vector3 end = pathPoints[numSegments - i];
+                Vector3 offset = (end - start) / growthInterval * Time.deltaTime;
+                for (var j = 0; j < numVertices; j++)
+                {
+                    vertices[i * numVertices + j] += offset;
+                }
+            }
+        }
+        ellapsedTime += Time.deltaTime;
+    }
+
+    /// <summary>
+    /// creates a growing mesh that extend along the path defined by pathPoints
+    /// over time.
+    /// Tip is the leading edge. New vertices are added to the tip
+    /// </summary>
+    void updateVineMeshPull()
+    {
+        vertices.Clear();
+        indices.Clear();
+
+        Vector3 start, end;
+
+        // use ellapsedTime to determine which segment to grow
+        int segment = Mathf.FloorToInt(ellapsedTime / growthInterval);
+        float t = ellapsedTime % growthInterval / growthInterval;
+        // print("ratio: " + ellapsedTime / growthInterval + " segment: " + segment + " t: " + t);
+
+        //FIXME: if t=0 or 1, the ring direction is wrong
+
+        //define tip position
+        start = pathPoints[segment];
+        end = pathPoints[segment + 1];
+        // Vector3 tip = Vector3.Lerp(start, end, t);
+        Vector3 tip = Vector3.Lerp(start, end, t);
+
+        makeRing(start, tip, true);
+        makeRing(tip, end, true);
+
+        // //segment n
+        // start = pathPoints[pathPoints.Length - 2];
+        // end = pathPoints[pathPoints.Length - 1];
+        // makeRing(start, end, false);
+
+        // for (int i = 0; i < pathPoints.Length - 1; i++)
+        // {
+        //     for (int j = 0; j < numVertices - 1; j++)
+        //     {
+        //         indices.AddRange(new int[] { i * numVertices + j, (i + 1) * numVertices + j, i * numVertices + j + 1 });
+        //         indices.AddRange(new int[] { (i + 1) * numVertices + j, (i + 1) * numVertices + j + 1, i * numVertices + j + 1 });
+        //     }
+        //     indices.AddRange(new int[] { (i + 1) * numVertices - 1, (i + 2) * numVertices - 1, i * numVertices });
+        //     indices.AddRange(new int[] { (i + 2) * numVertices - 1, (i + 1) * numVertices, i * numVertices });
+        // }
+
+        // mesh.Clear();
+        // mesh.vertices = vertices.ToArray();
+        // mesh.triangles = indices.ToArray();
+        // mesh.RecalculateNormals();
+
+        // ellapsedTime += Time.deltaTime;
+    }
+
+
+    /// <summary>
+    /// creates a cylinder mesh along the entire path defined by pathPoints
     /// </summary>
     void makeCylinderMesh()
     {
@@ -105,16 +207,25 @@ public class Vines : MonoBehaviour
                 {
                     Gizmos.color = Color.white;
                     Gizmos.DrawCube(transform.position + pathPoints[i], Vector3.one * radius * 0.1f);
-                    for (int j = 0; j < numVertices; j++)
-                    {
-                        Vector3 vertex = vertices[i * numVertices + j];
-                        Gizmos.color = Color.Lerp(Color.blue, Color.red, (float)j / numVertices);
-                        Gizmos.DrawSphere(transform.position + vertex, radius * 0.1f);
-                    }
+                    // for (int j = 0; j < numVertices; j++)
+                    // {
+                    //     Vector3 vertex = vertices[i * numVertices + j];
+                    //     Gizmos.color = Color.Lerp(Color.blue, Color.red, (float)j / numVertices);
+                    //     Gizmos.DrawSphere(transform.position + vertex, radius * 0.1f);
+                    // }
 
                 }
             }
 
+            if (vertices.Count > 0)
+            {
+                Gizmos.color = Color.cyan;
+                for (int j = 0; j < vertices.Count; j++)
+                {
+                    Vector3 vertex = vertices[j];
+                    Gizmos.DrawSphere(transform.position + vertex, radius * 0.1f);
+                }
+            }
         }
     }
 }
